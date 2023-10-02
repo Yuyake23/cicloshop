43ad73385bf4cc8f36b403f4180c1c9a3d622597
Yuyake23 <brunosamuel.orizona@gmail.com>
2023-10-02 09:08:20 -0300

    Adicionado arquivos do Spring

---
+++ src/main/java/edu/ifgoiano/trabalho/CicloshopApplication.java
@@ -0,0 +1,13 @@
+package edu.ifgoiano.trabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicloshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicloshopApplication.class, args);
+	}
+
+}
