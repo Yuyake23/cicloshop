59237ecce5c88b8a8656681dfe5a8115d64a26ec
Yuyake23 <brunosamuel.orizona@gmail.com>
2023-11-17 10:12:47 -0300

    Criada classe Peca

---
+++ src/main/java/edu/ifgoiano/trabalho/domain/Peca.java
@@ -0,0 +1,48 @@
+package edu.ifgoiano.trabalho.domain;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Peca extends Produto implements Serializable {
+
+	@Serial
+	private static final long serialVersionUID = 1L;
+
+	private String nome;
+	private Integer quantidade;
+
+	public Peca() {
+		super();
+	}
+
+	public Peca(Long id, Pessoa dono, String marca, BigDecimal precoCompra, String nome,
+			Integer quantidade) {
+		super(id, dono, marca, precoCompra);
+		this.nome = nome;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public String toString() {
+		return "Peca [id=" + id + "nome=" + nome + ", dono=" + dono + ", marca=" + marca + ", precoCompra="
+				+ precoCompra + ", quantidade=" + quantidade + "]";
+	}
+
+}
