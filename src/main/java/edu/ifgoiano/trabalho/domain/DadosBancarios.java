ca4d11b79ee62d8dd4a4b41147e4085fd38b9258
Yuyake23 <brunosamuel.orizona@gmail.com>
2023-11-17 10:14:27 -0300

    Criada classe DadosBancarios

---
+++ src/main/java/edu/ifgoiano/trabalho/domain/DadosBancarios.java
@@ -0,0 +1,100 @@
+package edu.ifgoiano.trabalho.domain;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class DadosBancarios implements Serializable {
+
+	@Serial
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String nomeBanco;
+	private String numeroAgencia;
+	private String tipoConta;
+	private String numeroConta;
+	private String operacao;
+
+	public DadosBancarios(Long id, String nomeBanco, String numeroAgencia, String tipoConta, String numeroConta,
+			String operacao) {
+		this.id = id;
+		this.nomeBanco = nomeBanco;
+		this.numeroAgencia = numeroAgencia;
+		this.tipoConta = tipoConta;
+		this.numeroConta = numeroConta;
+		this.operacao = operacao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeBanco() {
+		return nomeBanco;
+	}
+
+	public void setNomeBanco(String nomeBanco) {
+		this.nomeBanco = nomeBanco;
+	}
+
+	public String getNumeroAgencia() {
+		return numeroAgencia;
+	}
+
+	public void setNumeroAgencia(String numeroAgencia) {
+		this.numeroAgencia = numeroAgencia;
+	}
+
+	public String getTipoConta() {
+		return tipoConta;
+	}
+
+	public void setTipoConta(String tipoConta) {
+		this.tipoConta = tipoConta;
+	}
+
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public String getOperacao() {
+		return operacao;
+	}
+
+	public void setOperacao(String operacao) {
+		this.operacao = operacao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DadosBancarios other = (DadosBancarios) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "DadosBancarios [id=" + id + ", nomeBanco=" + nomeBanco + ", numeroAgencia=" + numeroAgencia
+				+ ", tipoConta=" + tipoConta + ", numeroConta=" + numeroConta + ", operacao=" + operacao + "]";
+	}
+
+}
