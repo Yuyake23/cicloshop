76712fbe762fc78d9896032d024047638442f92b
radialserial <natanael.alves.fernandes@gmail.com>
2023-11-28 20:42:34 -0300

    Adicionado Javadoc de PecaController

--- src/main/java/edu/ifgoiano/trabalho/controller/PecaController.java
+++ src/main/java/edu/ifgoiano/trabalho/controller/PecaController.java
@@ -1,68 +1,100 @@
 package edu.ifgoiano.trabalho.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import edu.ifgoiano.trabalho.dto.PecaDto;
 import edu.ifgoiano.trabalho.service.PecaService;
 import edu.ifgoiano.trabalho.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/peca")
 public class PecaController {
 
-	@Autowired
-	private ProdutoService produtoService;
-	@Autowired
-	private PecaService pecaService;
+    @Autowired
+    private ProdutoService produtoService;
+    @Autowired
+    private PecaService pecaService;
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public PecaDto salvar(@RequestBody PecaDto dto) {
-		return produtoService.salvar(dto);
-	}
-	
-	@PostMapping("/varias")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Iterable<PecaDto> salvarTodos(@RequestBody Iterable<PecaDto> dtos) {
-		return produtoService.salvarTodos(dtos);
-	}
-	
-	@GetMapping
-	public List<PecaDto> buscarTodos() {
-		return pecaService.buscarTodos();
-	}
-	
-	@GetMapping("/{id}")
-	public PecaDto buscarPorId(@PathVariable Long id) {
-		return pecaService.buscarPorId(id);
-	}
-	
-	@PutMapping("/{id}")
-	public PecaDto atualizarCompletamente(@PathVariable Long id, @RequestBody PecaDto dto) {
-		return pecaService.atualizarCompletamente(dto, id);
-	}
-	
-	@PatchMapping("/{id}")
-	public PecaDto atualizarParcialmente(@PathVariable Long id, @RequestBody PecaDto dto) {
-		return pecaService.atualizarParcialmente(dto, id);
-	}
-	
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deletarPorId(@PathVariable Long id) {
-		produtoService.deletarPorId(id);
-	}
+    /**
+     * Registra uma peça na base de dados.
+     *
+     * @param dto Um objeto representando a peça a ser registrada.
+     * @return A peça registrado na base de dados.
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PecaDto salvar(@RequestBody PecaDto dto) {
+        return produtoService.salvar(dto);
+    }
+
+    /**
+     * Registra várias peças na base de dados.
+     *
+     * @param dtos Uma lista de peças para serem registradas.
+     * @return A lista das peças registradas.
+     */
+    @PostMapping("/varias")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Iterable<PecaDto> salvarTodos(@RequestBody Iterable<PecaDto> dtos) {
+        return produtoService.salvarTodos(dtos);
+    }
+
+    /**
+     * Retorna todas as peças registradas.
+     *
+     * @return Uma lista de todas as peças registradas.
+     */
+    @GetMapping
+    public List<PecaDto> buscarTodos() {
+        return pecaService.buscarTodos();
+    }
+
+    /**
+     * Busca uma peça por seu id na base de dados.
+     *
+     * @param id O id da peça a ser buscada.
+     * @return A peça, caso esteja registrada.
+     */
+    @GetMapping("/{id}")
+    public PecaDto buscarPorId(@PathVariable Long id) {
+        return pecaService.buscarPorId(id);
+    }
+
+    /**
+     * Atualiza uma peça com dados novos, sobrescrevendo-a.
+     *
+     * @param id  O id da peça a ser sobrescrita.
+     * @param dto A peça que sobrescreverá a antiga.
+     * @return A peça entrada na base de dados.
+     */
+    @PutMapping("/{id}")
+    public PecaDto atualizarCompletamente(@PathVariable Long id, @RequestBody PecaDto dto) {
+        return pecaService.atualizarCompletamente(dto, id);
+    }
+
+    /**
+     * Modifica os dados de uma peça sem sobrescrevê-la.
+     *
+     * @param id  O id da peça a ser atualizada.
+     * @param dto Um objeto contendo os dados a serem modificados.
+     * @return Os dados da peça que foi atualizada.
+     */
+    @PatchMapping("/{id}")
+    public PecaDto atualizarParcialmente(@PathVariable Long id, @RequestBody PecaDto dto) {
+        return pecaService.atualizarParcialmente(dto, id);
+    }
+
+    /**
+     * Deleta uma peça do banco de dados por meio de seu id.
+     *
+     * @param id O id da peça a ser deletada.
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarPorId(@PathVariable Long id) {
+        produtoService.deletarPorId(id);
+    }
 }
