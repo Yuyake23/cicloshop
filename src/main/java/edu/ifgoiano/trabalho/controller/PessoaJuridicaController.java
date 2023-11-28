fb012d5b8bf10f506651edb2359822dbaaed88d1
radialserial <natanael.alves.fernandes@gmail.com>
2023-11-28 20:42:06 -0300

    Adicionado Javadoc de PessoaJuridicaController

--- src/main/java/edu/ifgoiano/trabalho/controller/PessoaJuridicaController.java
+++ src/main/java/edu/ifgoiano/trabalho/controller/PessoaJuridicaController.java
@@ -1,69 +1,101 @@
 package edu.ifgoiano.trabalho.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import edu.ifgoiano.trabalho.dto.PessoaJuridicaDto;
 import edu.ifgoiano.trabalho.service.PessoaJuridicaService;
 import edu.ifgoiano.trabalho.service.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/pessoa_juridica")
 public class PessoaJuridicaController {
-	
-	@Autowired
-	private PessoaService pessoaService;
-	@Autowired
-	private PessoaJuridicaService pessoaJuridicaService;
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public PessoaJuridicaDto salvar(@RequestBody PessoaJuridicaDto dto) {
-		return pessoaService.salvar(dto);
-	}
-	
-	@PostMapping("/varias")
-	@ResponseStatus(HttpStatus.CREATED)
-	public List<PessoaJuridicaDto> salvarTodos(@RequestBody List<PessoaJuridicaDto> dtos) {
-		return pessoaService.salvarTodos(dtos);
-	}
-	
-	@GetMapping
-	public List<? extends PessoaJuridicaDto> buscarTodos() {
-		return pessoaJuridicaService.buscarTodos();
-	}
-	
-	@GetMapping("/{id}")
-	public PessoaJuridicaDto buscarPorId(@PathVariable Long id) {
-		return pessoaJuridicaService.buscarPorId(id);
-	}
-	
-	@PutMapping("/{id}")
-	public PessoaJuridicaDto atualizarCompletamente(@PathVariable Long id, @RequestBody PessoaJuridicaDto dto) {
-		return pessoaJuridicaService.atualizarCompletamente(dto, id);
-	}
-	
-	@PatchMapping("/{id}")
-	public PessoaJuridicaDto atualizarParcialmente(@PathVariable Long id, @RequestBody PessoaJuridicaDto dto) {
-		return pessoaJuridicaService.atualizarParcialmente(dto, id);
-	}
-	
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deletarPorId(@PathVariable Long id) {
-		pessoaService.deletarPorId(id);
-	}
-	
+    @Autowired
+    private PessoaService pessoaService;
+    @Autowired
+    private PessoaJuridicaService pessoaJuridicaService;
+
+    /**
+     * Registra uma pessoa jurídica na base de dados.
+     *
+     * @param dto Um objeto representando a pessoa jurídica a ser registrada.
+     * @return A pessoa jurídica registrado na base de dados.
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PessoaJuridicaDto salvar(@RequestBody PessoaJuridicaDto dto) {
+        return pessoaService.salvar(dto);
+    }
+
+    /**
+     * Registra várias pessoas jurídicas na base de dados.
+     *
+     * @param dtos Uma lista de pessoas jurídicas para serem registradas.
+     * @return A lista das pessoas jurídicas registradas.
+     */
+    @PostMapping("/varias")
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<PessoaJuridicaDto> salvarTodos(@RequestBody List<PessoaJuridicaDto> dtos) {
+        return pessoaService.salvarTodos(dtos);
+    }
+
+    /**
+     * Retorna todas as pessoas jurídicas registradas.
+     *
+     * @return Uma lista de todas as pessoas jurídicas registradas.
+     */
+    @GetMapping
+    public List<? extends PessoaJuridicaDto> buscarTodos() {
+        return pessoaJuridicaService.buscarTodos();
+    }
+
+    /**
+     * Busca uma pessoa jurídica por seu id na base de dados.
+     *
+     * @param id O id da pessoa jurídica a ser buscada.
+     * @return A pessoa, caso esteja registrada.
+     */
+    @GetMapping("/{id}")
+    public PessoaJuridicaDto buscarPorId(@PathVariable Long id) {
+        return pessoaJuridicaService.buscarPorId(id);
+    }
+
+    /**
+     * Atualiza uma pessoa jurídica com dados novos, sobrescrevendo-a.
+     *
+     * @param id  O id da pessoa jurídica a ser sobrescrita.
+     * @param dto A pessoa jurídica que sobrescreverá a antiga.
+     * @return A pessoa jurídica entrada na base de dados.
+     */
+    @PutMapping("/{id}")
+    public PessoaJuridicaDto atualizarCompletamente(@PathVariable Long id, @RequestBody PessoaJuridicaDto dto) {
+        return pessoaJuridicaService.atualizarCompletamente(dto, id);
+    }
+
+    /**
+     * Modifica os dados de uma pessoa jurídica sem sobrescrevê-la.
+     *
+     * @param id  O id da pessoa jurídica a ser atualizada.
+     * @param dto Um objeto contendo os dados a serem modificados.
+     * @return Os dados da pessoa jurídica atualizada.
+     */
+    @PatchMapping("/{id}")
+    public PessoaJuridicaDto atualizarParcialmente(@PathVariable Long id, @RequestBody PessoaJuridicaDto dto) {
+        return pessoaJuridicaService.atualizarParcialmente(dto, id);
+    }
+
+    /**
+     * Deleta uma pessoa jurídica do banco de dados por meio de seu id.
+     *
+     * @param id O id da pessoa jurídica a ser deletada.
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarPorId(@PathVariable Long id) {
+        pessoaService.deletarPorId(id);
+    }
+
 }
