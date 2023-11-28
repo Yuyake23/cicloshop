dcaa8bb60ca5ec342dd07e8301b726428a024da5
radialserial <natanael.alves.fernandes@gmail.com>
2023-11-28 19:51:05 -0300

    Adicionado Javadoc de ContratoFuncionarioController

--- src/main/java/edu/ifgoiano/trabalho/controller/ContratoFuncionarioController.java
+++ src/main/java/edu/ifgoiano/trabalho/controller/ContratoFuncionarioController.java
@@ -1,68 +1,100 @@
 package edu.ifgoiano.trabalho.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import edu.ifgoiano.trabalho.dto.ContratoFuncionarioDto;
 import edu.ifgoiano.trabalho.service.ContratoFuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/contrato_funcionario")
 public class ContratoFuncionarioController {
 
-	@Autowired
-	private ContratoFuncionarioService contratoFuncionarioService;
+    @Autowired
+    private ContratoFuncionarioService contratoFuncionarioService;
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public ContratoFuncionarioDto salvar(@RequestBody ContratoFuncionarioDto dto) {
-		return contratoFuncionarioService.salvar(dto);
-	}
+    /**
+     * Registra um funcionário na base de dados.
+     *
+     * @param dto Um objeto representando o funcionário a ser registrado.
+     * @return O funcionário registrado na base de dados.
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContratoFuncionarioDto salvar(@RequestBody ContratoFuncionarioDto dto) {
+        return contratoFuncionarioService.salvar(dto);
+    }
 
-	@PostMapping("/varias")
-	@ResponseStatus(HttpStatus.CREATED)
-	public List<ContratoFuncionarioDto> salvarTodos(@RequestBody List<ContratoFuncionarioDto> dtos) {
-		return contratoFuncionarioService.salvarTodos(dtos);
-	}
+    /**
+     * Registra vários funcionários na base de dados.
+     *
+     * @param dtos Uma lista de funcionários para serem registrados.
+     * @return A lista dos funcionários registrados.
+     */
+    @PostMapping("/varias")
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<ContratoFuncionarioDto> salvarTodos(@RequestBody List<ContratoFuncionarioDto> dtos) {
+        return contratoFuncionarioService.salvarTodos(dtos);
+    }
 
-	@GetMapping
-	public List<ContratoFuncionarioDto> buscarTodos() {
-		return contratoFuncionarioService.buscarTodos();
-	}
+    /**
+     * Retorna todos os funcionários registrados.
+     *
+     * @return Uma lista de todos os funcionários registrados.
+     */
+    @GetMapping
+    public List<ContratoFuncionarioDto> buscarTodos() {
+        return contratoFuncionarioService.buscarTodos();
+    }
 
-	@GetMapping("/{id}")
-	public ContratoFuncionarioDto buscarPorId(@PathVariable Long id) {
-		return contratoFuncionarioService.buscarPorId(id);
-	}
+    /**
+     * Busca um funcionário por seu id na base de dados.
+     *
+     * @param id O id do funcionário a ser buscado.
+     * @return O funcionário, caso esteja registrado.
+     */
+    @GetMapping("/{id}")
+    public ContratoFuncionarioDto buscarPorId(@PathVariable Long id) {
+        return contratoFuncionarioService.buscarPorId(id);
+    }
 
-	@PutMapping("/{id}")
-	public ContratoFuncionarioDto atualizarCompletamente(@PathVariable Long id,
-			@RequestBody ContratoFuncionarioDto dto) {
-		return contratoFuncionarioService.atualizarCompletamente(dto, id);
-	}
+    /**
+     * Atualiza um funcionário com dados novos, sobrescrevendo-o.
+     *
+     * @param id  O id do funcionário a ser sobrescrito.
+     * @param dto O funcionário que sobrescreverá o antigo.
+     * @return O funcionário que foi entrado na base de dados.
+     */
+    @PutMapping("/{id}")
+    public ContratoFuncionarioDto atualizarCompletamente(@PathVariable Long id,
+                                                         @RequestBody ContratoFuncionarioDto dto) {
+        return contratoFuncionarioService.atualizarCompletamente(dto, id);
+    }
 
-	@PatchMapping("/{id}")
-	public ContratoFuncionarioDto atualizarParcialmente(@PathVariable Long id,
-			@RequestBody ContratoFuncionarioDto dto) {
-		return contratoFuncionarioService.atualizarParcialmente(dto, id);
-	}
+    /**
+     * Modifica os dados de um funcionário sem sobrescrevê-lo.
+     *
+     * @param id  O id do funcionário a ser atualizado.
+     * @param dto Um objeto contendo os dados a serem modificados.
+     * @return Os dados do funcionário que foi atualizado.
+     */
+    @PatchMapping("/{id}")
+    public ContratoFuncionarioDto atualizarParcialmente(@PathVariable Long id,
+                                                        @RequestBody ContratoFuncionarioDto dto) {
+        return contratoFuncionarioService.atualizarParcialmente(dto, id);
+    }
 
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deletarPorId(@PathVariable Long id) {
-		contratoFuncionarioService.deletarPorId(id);
-	}
+    /**
+     * Deleta um funcionário do banco de dados por meio de seu id.
+     *
+     * @param id O id do funcionário a ser deletado.
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarPorId(@PathVariable Long id) {
+        contratoFuncionarioService.deletarPorId(id);
+    }
 
 }
