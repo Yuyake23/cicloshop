1110c3dc26010dd5e4ef3d7bcb8897780daa21db
radialserial <natanael.alves.fernandes@gmail.com>
2023-12-02 00:53:36 -0300

    Adicionada configuração de segurança + filtro JWT

---
+++ src/main/java/edu/ifgoiano/trabalho/config/JwtAuthFilter.java
@@ -0,0 +1,71 @@
+package edu.ifgoiano.trabalho.config;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+import edu.ifgoiano.trabalho.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/** Classe para autenticação de JWT. */
+@RequiredArgsConstructor
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+  private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
+
+  @Override
+  protected void doFilterInternal(
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain)
+      throws ServletException, IOException {
+    final String authHeader = request.getHeader(AUTHORIZATION);
+    final String token;
+    final String username;
+
+    // Checar se o token está no header
+    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    // Extraindo o token
+    // Pular 7 caracteres por causa de "Bearer "
+    token = authHeader.substring(7);
+    username = jwtService.extrairUsername(token);
+
+    // Checando se usuário já está logado
+    // FIXME Contexto não está segurando autorização, retorna 403 mesmo se usar token
+    SecurityContext securityContext = SecurityContextHolder.getContext();
+    if (username != null && securityContext.getAuthentication() == null) {
+
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+      if (jwtService.tokenEstaValido(token, userDetails)) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+            new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        securityContext.setAuthentication(authenticationToken);
+      }
+    }
+
+    filterChain.doFilter(request, response);
+  }
+}
